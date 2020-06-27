64a5453767bd2dfe205b4e6b7c4826d998efb14d
Ana Arreola <laurarreola.ana@gmail.com>
2020-06-26 20:17:05 -0500

    Adding more tests Testing rules of algorithm

--- minitest.rb
+++ minitest.rb
@@ -9,12 +9,27 @@ class CreatesBoard
         result = @board == Array
         assert_equal result, Array
   end
-  def is_rule_one_ok 
+
+  def is_rule_one_ok(true)
+    assert(false)
+    assert(status_cell(row,column) == true && neighbours_count <2 
+    change_cell_state(row,column)) {
+    test= false
+    }
   end
 
-  def is_rule_two_ok 
+  def is_rule_two_ok (true)
+    assert(true)
+    assert(status_cell(row,column) == true && neighbours_count  ==2 || neighbours_count == 3){
+    test= true
+  }
   end
-  def is_rule_three_ok 
+  def is_rule_three_ok (false)
+    assert(false)
+    assert(status_cell(row,column) == false && neighbours_count  == 1 || neighbours_count >= 4
+    change_cell_state(row,column)){
+        test= true
+    }
   end
 end
 

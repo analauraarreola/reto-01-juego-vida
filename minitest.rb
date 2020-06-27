a13a5c5acdbf885666f4ac8c32a8f643c89bd50d
Ana Arreola <laurarreola.ana@gmail.com>
2020-06-26 20:23:50 -0500

    Final test Testing class and conditionals

--- minitest.rb
+++ minitest.rb
@@ -12,7 +12,7 @@ class CreatesBoard
 
   def is_rule_one_ok(true)
     assert(false)
-    assert(status_cell(row,column) == true && neighbours_count <2 
+    if status_cell(row,column) == true && neighbours_count <2 
     change_cell_state(row,column)) {
     test= false
     }
@@ -20,32 +20,15 @@ class CreatesBoard
 
   def is_rule_two_ok (true)
     assert(true)
-    assert(status_cell(row,column) == true && neighbours_count  ==2 || neighbours_count == 3){
+    if status_cell(row,column) == true && neighbours_count  ==2 || neighbours_count == 3){
     test= true
   }
   end
   def is_rule_three_ok (false)
     assert(false)
-    assert(status_cell(row,column) == false && neighbours_count  == 1 || neighbours_count >= 4
+    if status_cell(row,column) == false && neighbours_count  == 1 || neighbours_count >= 4
     change_cell_state(row,column)){
         test= true
     }
   end
 end
-
-def rules
-    create_matrix 
-    (0...10).each do |row|
-      (0...10).each do |column|
-        neighbours_count = neighbours(row,column) #Decidiendo si cada célula vive, revive, muere o se reproduce. 
-            #Una célula viva  que tenga menos de 2 vecinas vivos, muere
-        if status_cell(row,column) == true && neighbours_count <2 
-          change_cell_state(row,column)
-              #Una célula viva con dos o tres vecinas vivas pasa a la siguiente generación, vive
-        elsif status_cell(row,column) == true && neighbours_count  ==2 || neighbours_count == 3 
-              #Una célula viva, si sus vecinos vivos son 1 o 4 +
-        else status_cell(row,column) == false && neighbours_count  == 1 || neighbours_count >= 4
-          change_cell_state(row,column)
-        end
-      end
-    end
